3510dd7c6ae4994dcddcb8cb36648e095e8c7208
Sahil Khan <sahkhan@microsoft.com>
2024-02-02 15:31:35 -0800

    First Hello World Program

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include "HelloWorld.h"
+int main()
+{
+    PRINT_INFO("Hello World!");
+    PRINT_WARN("Hello World!");
+    PRINT_ERROR("Hello World!");
+}
---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#define PRINT_INFO(str)		std::cout << "[INFO] " << str << std::endl
+#define PRINT_ERROR(str)	std::cout << "[ERROR] " << str << std::endl
+#define PRINT_WARN(str)		std::cout << "[WARN] " << str << std::endl
+
