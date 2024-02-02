6e5c74dcbd57dcf31805e0ce6728c78ab22e6d0f
Sahil Khan <sahkhan@microsoft.com>
2024-02-02 15:35:42 -0800

    Added the std namespace in HelloWorld.h file

--- HelloWorld/HelloWorld.h
+++ HelloWorld/HelloWorld.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
-#define PRINT_INFO(str)		std::cout << "[INFO] " << str << std::endl
-#define PRINT_ERROR(str)	std::cout << "[ERROR] " << str << std::endl
-#define PRINT_WARN(str)		std::cout << "[WARN] " << str << std::endl
+using namespace std;
+
+#define PRINT_INFO(str)		cout << "[INFO] " << str << endl
+#define PRINT_ERROR(str)	cout << "[ERROR] " << str << endl
+#define PRINT_WARN(str)		cout << "[WARN] " << str << endl
 
